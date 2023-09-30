ea765da467f5961e97918aacdf216401652002c2
kanta <yasuda.kanta.516@gmail.com>
2023-09-30 13:35:46 +0000

    import limits

--- services/ap/app/mymodule.py
+++ services/ap/app/mymodule.py
@@ -5,6 +5,7 @@ from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 from time import time
 from datetime import time as create_time
+import limits
 
 AVOID_API = os.environ.get('AVOID_API') in ('1', 1, 'True', True)
 
@@ -21,12 +22,20 @@ def get_const_value(const_name: str) -> str:
   else:
     raise ValueError(f'Varievle {const_name} is not defined.')
 
+REDIS_HOST = os.environ['REDIS_HOST']
+REDIS_PORT = os.environ['REDIS_PORT']
+REDIS_PASSWORD = get_const_value('REDIS_PASSWORD')
+REDIS_DB = get_const_value('REDIS_DATABASE')       # ページキャッシュ用
+REDIS_DB2 = get_const_value('REDIS_DATABASE2')     # API呼び出し監視用
+# REDIS_DB3 = get_const_value('REDIS_DATABASE3')     # 発音データのキャッシュ用
+
 class WordsAPI():
   headers = {
     'X-RapidAPI-Key': get_const_value('WORDSAPI_KEY'),
     'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
   }
   i = 1
+
   logger = getLogger(f'{__name__}::WordsAPI')
   logger.addHandler(TimedRotatingFileHandler(
     filename=LOG_DIR / 'wordsapi.log', 
@@ -38,32 +47,47 @@ class WordsAPI():
   ))
   logger.setLevel(INFO)
 
-  def get_pronunciation(self, word):
-    if AVOID_API:
-      self.logger.info(f'time:{time():.9f}\tword:{word}\tres_code:999\tres_sec:0.000000\tpron:-')
-      return '-'
-    url = f'https://wordsapiv1.p.rapidapi.com/words/{word}/pronunciation'
-    time_request = time()
-    res = requests.get(url, headers=self.headers)
-    res_sec = time() - time_request
-    self.i += 1
+  storage = limits.storage.RedisStorage(f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB2}')
+  limiter = limits.strategies.MovingWindowRateLimiter(storage)
+  rate_limit = limits.parse('200/2hours')
 
+  def get_pronunciation(self, word) -> str:
     pron = '-'
-    decoded = True
+
+    AVOID_API = False         # 必ず消す！！！☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★
+    if AVOID_API or not self.limiter.hit(self.rate_limit, 'WordsAPI'):
+      time_request = time()
+      res_code = 429
+      res_sec = 0.
+    else:
+      pron = 'success'
+      time_request = time()
+      res_code = 2200
+      res_sec = 0.
+
+      # url = f'https://wordsapiv1.p.rapidapi.com/words/{word}/pronunciation'
+      # time_request = time()
+      # res = requests.get(url, headers=self.headers)
+      # res_sec = time() - time_request
+      # res_code = res.status_code
+
+      # self.i += 1
+
+      # decoded = True
+      
+      # try:
+      #   data = res.json()
+      # except requests.exceptions.JSONDecodeError:
+      #   decoded = False
+      
+      # if decoded:
+      #   data = data[0] if isinstance(data, list) else data
+      #   if isinstance(data, dict):
+      #     prons = data.get('pronunciation', pron)
+      #     if isinstance(prons, dict):
+      #       pron = data.get('all', pron)
+      #     elif isinstance(prons, str):
+      #       pron = prons
     
-    try:
-      data = res.json()
-    except requests.exceptions.JSONDecodeError:
-      decoded = False
-    
-    if decoded:
-      data = data[0] if isinstance(data, list) else data
-      if isinstance(data, dict):
-        prons = data.get('pronunciation', pron)
-        if isinstance(prons, dict):
-          pron = data.get('all', pron)
-        elif isinstance(prons, str):
-          pron = prons
-    
-    self.logger.info(f'time:{time_request:.9f}\tword:{word}\tres_code:{res.status_code}\tres_sec:{res_sec:.6f}\tpron:{pron}')
+    self.logger.info(f'time:{time_request:.9f}\tword:{word}\tres_code:{res_code}\tres_sec:{res_sec:.6f}\tpron:{pron}')
     return pron
