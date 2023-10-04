9ccbbffad1215bcdc1ba20190003a6d65b41be45
kanta <yasuda.kanta.516@gmail.com>
2023-10-04 13:42:06 +0900

    fix bug of reload_csv

--- services/db/mysql-files/sh/reload_csv.sh
+++ services/db/mysql-files/sh/reload_csv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
 # 実行方法: $ <このスクリプトのパス> <テーブル名>
 # なお、このスクリプトはどこに移動しても問題なく動きます。
+set -eu
 
 # 引数チェック
 if [ $# != 1 ]; then
@@ -12,5 +12,6 @@ fi
 # メイン
 sql_files=/var/lib/mysql-files/sql
 rootpw=$(sed -n 1p $MYSQL_ROOT_PASSWORD_FILE)
-mysql -u root -p$rootpw $MYSQL_DATABASE -e "$(sed s/%tableName%/${1}/g ${sql_files}/_update_base.sql)"
+dbdb=$(sed -n 1p $MYSQL_DATABASE_FILE)
+mysql -u root -p$rootpw $dbdb -e "$(sed s/%tableName%/${1}/g ${sql_files}/_update_base.sql)"
 echo "Successfully reloaded csv!"
