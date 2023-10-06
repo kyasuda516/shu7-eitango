60f24b11ce302d1edc0cc0bed937709dabc07736
kanta <yasuda.kanta.516@gmail.com>
2023-10-06 12:13:53 +0000

    correct initialize.sh

--- services/db/initialize.sh
+++ services/db/initialize.sh
@@ -2,12 +2,13 @@
 set -eu
 
 sql_files=/var/lib/mysql-files/sql
+stmt=""
 stmt="${stmt}  source ${sql_files}/set_procedures.sql;"
 stmt="${stmt}  source ${sql_files}/define_bases.sql;"
 stmt="${stmt}  $(sed s/%tableName%/pos_config/g ${sql_files}/_update_base.sql)"
 stmt="${stmt}  $(sed s/%tableName%/pool/g ${sql_files}/_update_base.sql)"
 stmt="${stmt}  source ${sql_files}/set_bunches.sql;"
-stmt="${stmt}  source ${sql_files}/set_event.sql;"
+stmt="${stmt}  source ${sql_files}/set_events.sql;"
 
 stmt="${stmt}  REVOKE ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* FROM \`${MYSQL_USER}\`@\`%\`;"
 stmt="${stmt}  GRANT SELECT ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@\`%\`;"
